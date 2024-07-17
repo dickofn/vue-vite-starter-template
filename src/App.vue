ce260f43ba995fbe042e4ea0409fc4d8da072e62
Dicko Finus <dickofn@gmail.com>
2024-07-17 13:39:24 +0700

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-red-900 text-green-200">{{ msg }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const msg = ref("Hello World");
+</script>
+
+<style scoped></style>
